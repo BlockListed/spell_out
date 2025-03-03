1b095080753ab76a5521aaa62d4dbf96b83366eb
BlockListed <44610569+BlockListed@users.noreply.github.com>
2025-03-03 23:14:17 +0100

    turns out RTFM does help

--- flake.nix
+++ flake.nix
@@ -1,11 +1,13 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
   outputs =
     {
       self,
       nixpkgs,
+      nixpkgs-unstable,
     }:
     let
       systems = [
@@ -16,40 +18,17 @@
       forAllSystems = nixpkgs.lib.genAttrs systems;
     in
     {
-      packages.x86_64-linux.gleam =
-        let
-          pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        in
-        pkgs.stdenv.mkDerivation rec {
-          pname = "gleam";
-          version = "1.8.1";
-
-          src = pkgs.fetchzip {
-            url = "https://github.com/gleam-lang/gleam/releases/download/v${version}/gleam-v${version}-x86_64-unknown-linux-musl.tar.gz";
-            sha256 = "sha256-pVpjB7GOQSgxJLH3Y+NteHM2a3lXi8tX3DyrLJ1bXv8=";
-          };
-
-          installPhase = ''
-            mkdir -p $out/bin
-            install -m 755 $src/gleam $out/bin
-          '';
-
-          meta = with nixpkgs.lib; {
-            description = "Gleam!";
-            license = licenses.asl20;
-            platforms = platforms.linux;
-          };
-        };
       devShells = forAllSystems (
         system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
+          pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
         in
         {
           default = pkgs.mkShellNoCC {
             packages = [
               pkgs.git
-              self.packages.${system}.gleam
+              pkgs-unstable.gleam
               pkgs.erlang_27
             ];
 
