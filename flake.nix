502ce1e0da570d518a7252390ac8b81554685aac
BlockListed <44610569+BlockListed@users.noreply.github.com>
2025-03-03 21:09:49 +0100

    nix flake latest version

--- flake.nix
+++ flake.nix
@@ -16,6 +16,30 @@
       forAllSystems = nixpkgs.lib.genAttrs systems;
     in
     {
+      packages.x86_64-linux.gleam =
+        let
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        in
+        pkgs.stdenv.mkDerivation rec {
+          pname = "gleam";
+          version = "1.8.1";
+
+          src = pkgs.fetchzip {
+            url = "https://github.com/gleam-lang/gleam/releases/download/v${version}/gleam-v${version}-x86_64-unknown-linux-musl.tar.gz";
+            sha256 = "sha256-pVpjB7GOQSgxJLH3Y+NteHM2a3lXi8tX3DyrLJ1bXv8=";
+          };
+
+          installPhase = ''
+            mkdir -p $out/bin
+            install -m 755 $src/gleam $out/bin
+          '';
+
+          meta = with nixpkgs.lib; {
+            description = "Gleam!";
+            license = licenses.asl20;
+            platforms = platforms.linux;
+          };
+        };
       devShells = forAllSystems (
         system:
         let
@@ -25,8 +49,13 @@
           default = pkgs.mkShellNoCC {
             packages = [
               pkgs.git
-              pkgs.gleam
+              self.packages.${system}.gleam
+              pkgs.erlang_27
             ];
+
+            shellHook = ''
+              export SHELL="${pkgs.bashInteractive}/bin/bash"
+            '';
           };
         }
       );
